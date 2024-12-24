f0fa58f744edb2c95c8e5636809cdcec72a7ea37
jorge111 <48180592+Ambidiosidad@users.noreply.github.com>
2024-12-24 15:56:39 +0100

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,8 @@
 // https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
+    title: 'NextJS, por Jorge Simon',
     description: 'NextJS tutorial',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     aside: {
       level: 0,
       collapsed: false,
